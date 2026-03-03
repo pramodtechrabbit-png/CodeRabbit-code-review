beded554fc037f9730075101a937381b8452ed99
pramodtechrabbit-png <pramodtechrabbit@gmail.com>
2026-03-03 13:40:57 +0530

    Correct username reference in reset_password function
    Fix incorrect variable reference in password reset function.

--- buggy_login_app.py
+++ buggy_login_app.py
@@ -101,7 +101,7 @@ def add_user(username, password):
 
 def reset_password(username, new_password):
     for user in USERS:
-        if user["username"] == username:
+        if user["username"] == pramod:
             user["password"] = new_password
             log(f"Reset password for {username} to {new_password}")
             return True
